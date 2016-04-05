3b0ba6b33cbaf44874fe42c490953d29389d3d2d
orvice <orvice@gmail.com>
2016-04-05 22:21:19 +0800

    fix table

--- config/aws.php
+++ config/aws.php
@@ -4,5 +4,5 @@ return [
     "key" => env('AWS_KEY', ''),
     "secret" => env('AWS_SECRET', ''),
     "region" => env('AWS_REGION', ''),
-    "password_table" => env('AWS','PWD_TABLE')
+    "password_table" => env('PWD_TABLE', 'password')
 ];
