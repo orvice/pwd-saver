6a54769d29f7418e43713832cffc63fe9aa1f2d7
orvice <orvice@gmail.com>
2016-04-06 13:02:20 +0800

    config sqs key in .env

--- config/queue.php
+++ config/queue.php
@@ -51,8 +51,8 @@ return [
 
         'sqs' => [
             'driver' => 'sqs',
-            'key' => 'your-public-key',
-            'secret' => 'your-secret-key',
+            "key" => env('AWS_KEY', ''),
+            "secret" => env('AWS_SECRET', ''),
             'prefix' => 'https://sqs.us-east-1.amazonaws.com/your-account-id',
             'queue' => 'your-queue-name',
             'region' => 'us-east-1',
